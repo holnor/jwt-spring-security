c1d9817a064068724e3c3e688b6f692946e918cf
holczur <holcz.norbert@gmail.com>
2023-07-02 18:22:47 +0200

    Methods was removed

--- src/main/java/hu/holnor/register/service/AccountService.java
+++ src/main/java/hu/holnor/register/service/AccountService.java
@@ -6,40 +6,15 @@ import hu.holnor.register.repository.AccountRepository;
 import jakarta.persistence.EntityExistsException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class AccountService {
-    private AccountRepository accountRepository;
-    private PasswordEncoder passwordEncoder;
+    private final AccountRepository accountRepository;
 
     @Autowired
-    public AccountService(AccountRepository accountRepository, PasswordEncoder passwordEncoder) {
+    public AccountService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    public void registerAccount(AccountCommand accountCommand) throws EntityExistsException {
-        String username = accountCommand.getUsername();
-        String email = accountCommand.getEmail();
-        String password = accountCommand.getPassword();
-
-        if (findAccountByEmail(email) == null && findAccountByUsername(username) == null) {
-            Account account = new Account(username, email, password);
-            account.setPassword(passwordEncoder.encode(password));
-            accountRepository.save(account);
-        } else {
-            throw new EntityExistsException();
-        }
-    }
-
-    private Account findAccountByUsername(String username) {
-        return accountRepository.findAccountByUsername(username).orElse(null);
-    }
-
-    private Account findAccountByEmail(String email) {
-        return accountRepository.findAccountByEmail(email).orElse(null);
     }
 }
