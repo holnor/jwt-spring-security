73717dfcb435d3cee81c8876d56795705f83e42c
holczur <holcz.norbert@gmail.com>
2023-07-02 18:24:11 +0200

    Role was introdueced

---
+++ src/main/java/hu/holnor/register/config/Role.java
@@ -0,0 +1,17 @@
+package hu.holnor.register.config;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
