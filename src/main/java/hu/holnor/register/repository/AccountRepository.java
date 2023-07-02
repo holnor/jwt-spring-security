93b5a4ee577eac5792dcd16d23b95fcd4af9fc12
holczur <holcz.norbert@gmail.com>
2023-07-02 18:22:16 +0200

    queries that return boolean if user exist was added

--- src/main/java/hu/holnor/register/repository/AccountRepository.java
+++ src/main/java/hu/holnor/register/repository/AccountRepository.java
@@ -2,13 +2,13 @@ package hu.holnor.register.repository;
 
 import hu.holnor.register.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    public Optional<Account> findAccountByUsername(String username);
-    public Optional<Account> findAccountByEmail(String email);
+    Optional<Account> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
